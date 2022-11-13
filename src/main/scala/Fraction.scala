b4abafa51810b5119e6f104d9ed591bcb1a9dde1
Blake Ramsdell <blaker@gmail.com>
2022-11-12 18:25:54 -0800

    Add toFraction String extension method

--- src/main/scala/Fraction.scala
+++ src/main/scala/Fraction.scala
@@ -1,1 +1,7 @@
 case class Fraction(numerator: Int, denominator: Int)
+
+// https://docs.scala-lang.org/scala3/reference/contextual/extension-methods.html
+extension (s: String)
+    def toFraction: Fraction = {
+        return Fraction(s.toInt, 1)
+    }
--- src/test/scala/FractionSuite.scala
+++ src/test/scala/FractionSuite.scala
@@ -3,4 +3,10 @@ class FractionSuite extends munit.FunSuite {
     val fraction = Fraction(1, 2)
     assert(fraction != null)
   }
+
+  test("toFraction String extension method whole number") {
+    val obtained = "1".toFraction
+    val expected = Fraction(1, 1)
+    assertEquals(obtained, expected)
+  }
 }
