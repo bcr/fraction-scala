bd53abe3be0cfbce5253e9f453f2bc98891e3be3
Blake Ramsdell <blaker@gmail.com>
2022-11-12 20:00:58 -0800

    Made things look more Scala

--- src/main/scala/Fraction.scala
+++ src/main/scala/Fraction.scala
@@ -1,16 +1,14 @@
-private def gcf(a: Int, b: Int) : Int = {
+private def gcf(a: Int, b: Int) : Int =
     var temp = 0
     var num1 = a
     var num2 = b
 
     while(num2 != 0)
-    {
         temp = num1 % num2
         num1 = num2
         num2 = temp
-    }
+
     return num1
-}
 
 case class Fraction(numerator: Int, denominator: Int):
     def *(that: Fraction) = Fraction(this.numerator * that.numerator, this.denominator * that.denominator)
@@ -21,28 +19,23 @@ case class Fraction(numerator: Int, denominator: Int):
         val this_gcf = gcf(numerator, denominator)
         Fraction(numerator / this_gcf, denominator / this_gcf)
     def toMixedNumberString: String =
-        if (denominator == 1) {
+        if (denominator == 1)
             return s"${numerator / denominator}"
-        }
-        else {
+        else
             return s"${numerator}/${denominator}"
-        }
 
 // https://docs.scala-lang.org/scala3/reference/contextual/extension-methods.html
 extension (s: String)
-    def toFraction: Fraction = {
+    def toFraction: Fraction =
         var whole = 0
         var remainder: String = s;
         val underscore_pieces = s.split('_')
-        if (underscore_pieces.length > 1) {
+        if (underscore_pieces.length > 1)
             whole = underscore_pieces(0).toInt
             remainder = underscore_pieces(1)
-        }
         val fraction_pieces = remainder.split('/')
-        if (fraction_pieces.length > 1) {
+        if (fraction_pieces.length > 1)
             val numerator = fraction_pieces(0).toInt
             val denominator = fraction_pieces(1).toInt
             return Fraction(numerator + whole * denominator, denominator)
-        }
         return Fraction(remainder.toInt, 1)
-    }
